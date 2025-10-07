9d8da48615d34307cd33097c1cea82078de03dd6
333IAN <iisavwa333@gmail.com>
2025-10-07 13:27:38 -0600

    Resend changes

--- portcontact/portcontact/settings.py
+++ portcontact/portcontact/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-dev')
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-ALLOWED_HOSTS = ['my-portfolio-us.up.railway.app', 'localhost', '127.0.0.1', '.railway.app']
+
 
 # Applications
 INSTALLED_APPS = [
@@ -78,13 +78,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Email configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST','smtp.gmail.com')
-EMAIL_PORT = os.getenv('EMAIL_PORT',587)
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS','True').lower() == 'true'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER','')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD','')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL',EMAIL_HOST_USER)
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', '')
 
 # Rest of your settings...
 ROOT_URLCONF = 'portcontact.urls'
